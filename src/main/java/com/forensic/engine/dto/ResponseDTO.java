943b5f6dd8adf077396c9233e900d53372ef3d43
Harisankar <harisankar2206@gmail.com>
2026-01-16 12:09:38 +0530

    Update ResponseDTO.java

--- src/main/java/com/forensic/engine/dto/ResponseDTO.java
+++ src/main/java/com/forensic/engine/dto/ResponseDTO.java
@@ -1,1 +1,10 @@
+package com.forensic.engine.dto;
+import lombok.Builder;
+import lombok.Data;
 
+@Data @Builder
+public class ResponseDTO {
+    private double aiProbability;
+    private String verdict;
+    private String forensicDetails; // Detailed explanation of why
+}

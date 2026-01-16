e7b30d6069b0ba3d9c657e0c016477fe14bd0d7a
Harisankar <harisankar2206@gmail.com>
2026-01-16 12:11:09 +0530

    Update CodeParserUtils.java

--- src/main/java/com/forensic/engine/util/CodeParserUtils.java
+++ src/main/java/com/forensic/engine/util/CodeParserUtils.java
@@ -1,1 +1,11 @@
+package com.forensic.engine.util;
 
+public class CodeParserUtils {
+    public static String cleanCode(String rawCode) {
+        if (rawCode == null) return "";
+        // Remove single line comments and extra whitespace
+        return rawCode.replaceAll("//.*", "")
+                      .replaceAll("\\s+", " ")
+                      .trim();
+    }
+}

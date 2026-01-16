d63524aff7b3c4865ca69e049ab0697e3f9f82cb
Harisankar <harisankar2206@gmail.com>
2026-01-16 12:18:03 +0530

    Update ForensicAnalysisException.java

--- src/main/java/com/forensic/engine/exception/ForensicAnalysisException.java
+++ src/main/java/com/forensic/engine/exception/ForensicAnalysisException.java
@@ -1,1 +1,7 @@
+package com.forensic.engine.exception;
 
+public class ForensicAnalysisException extends RuntimeException {
+    public ForensicAnalysisException(String message) {
+        super(message);
+    }
+}

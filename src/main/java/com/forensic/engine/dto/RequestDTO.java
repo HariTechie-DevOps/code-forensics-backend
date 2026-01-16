b280a2936a76cf1933afc0c02ed523a0c93def9a
Harisankar <harisankar2206@gmail.com>
2026-01-16 12:09:17 +0530

    Update RequestDTO.java

--- src/main/java/com/forensic/engine/dto/RequestDTO.java
+++ src/main/java/com/forensic/engine/dto/RequestDTO.java
@@ -8,14 +8,4 @@ public class RequestDTO {
     private String language; // e.g., "java", "python"
 }
 
-// ResponseDTO.java
-package com.forensic.engine.dto;
-import lombok.Builder;
-import lombok.Data;
 
-@Data @Builder
-public class ResponseDTO {
-    private double aiProbability;
-    private String verdict;
-    private String forensicDetails; // Detailed explanation of why
-}

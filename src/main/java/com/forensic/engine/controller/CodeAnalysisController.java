33b23412000c1a76dee91dcc4cad01f02b3fd4c7
Harisankar <harisankar2206@gmail.com>
2026-01-16 12:05:18 +0530

    Update CodeAnalysisController.java

--- src/main/java/com/forensic/engine/controller/CodeAnalysisController.java
+++ src/main/java/com/forensic/engine/controller/CodeAnalysisController.java
@@ -1,1 +1,18 @@
+package com.forensic.engine.controller;
 
+import com.forensic.engine.entity.AnalysisReport;
+import com.forensic.engine.service.AnalysisService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/forensics")
+@RequiredArgsConstructor
+public class CodeAnalysisController {
+    private final AnalysisService service;
+
+    @PostMapping("/analyze")
+    public AnalysisReport postAnalysis(@RequestBody String code) {
+        return service.analyze(code);
+    }
+}

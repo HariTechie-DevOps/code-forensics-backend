95469fbeb989e3404b1c1d02282c7b0f594771a9
Harisankar <harisankar2206@gmail.com>
2026-01-16 12:11:55 +0530

    Update AnalysisRepository.java

--- src/main/java/com/forensic/engine/repository/AnalysisRepository.java
+++ src/main/java/com/forensic/engine/repository/AnalysisRepository.java
@@ -1,1 +1,10 @@
+package com.forensic.engine.repository;
 
+import com.forensic.engine.entity.AnalysisReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnalysisRepository extends JpaRepository<AnalysisReport, Long> {
+    // Standard CRUD (Save, Find, Delete) is handled by JpaRepository
+}

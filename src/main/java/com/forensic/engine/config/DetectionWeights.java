5209f1521fa9019224e918f32a0c6c55137a832a
Harisankar <harisankar2206@gmail.com>
2026-01-16 12:02:16 +0530

    Update DetectionWeights.java

--- src/main/java/com/forensic/engine/config/DetectionWeights.java
+++ src/main/java/com/forensic/engine/config/DetectionWeights.java
@@ -1,1 +1,13 @@
+package com.forensic.engine.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "forensics.weights")
+public class DetectionWeights {
+    private double grammarPerfection;
+    private double logicDensity;
+}

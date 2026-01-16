e199c880cfc0fe8420844b435d0297d1c523fa0b
Harisankar <harisankar2206@gmail.com>
2026-01-16 12:10:38 +0530

    Update GlobalExceptionHandler.java

--- src/main/java/com/forensic/engine/exception/GlobalExceptionHandler.java
+++ src/main/java/com/forensic/engine/exception/GlobalExceptionHandler.java
@@ -1,1 +1,14 @@
+package com.forensic.engine.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntime(RuntimeException ex) {
+        return ResponseEntity.internalServerError().body("Forensic Error: " + ex.getMessage());
+    }
+}

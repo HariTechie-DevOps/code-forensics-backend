87b34d423b86da26fb10f2f0a79c3fdace37d49e
Harisankar <harisankar2206@gmail.com>
2026-01-16 11:44:56 +0530

    Initialize Spring Boot application structure

---
+++ src/main/java/com/forensic/engine/ForensicApplication.java
@@ -0,0 +1,11 @@
+package com.forensic.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForensicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ForensicApplication.class, args);
+    }
+}

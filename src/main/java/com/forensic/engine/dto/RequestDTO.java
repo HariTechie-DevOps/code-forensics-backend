4508769ce4418932ded500a8982f8b8744f8c7a6
Harisankar <harisankar2206@gmail.com>
2026-01-18 10:07:45 +0530

    Update RequestDTO.java

--- src/main/java/com/forensic/engine/dto/RequestDTO.java
+++ src/main/java/com/forensic/engine/dto/RequestDTO.java
@@ -1,10 +1,16 @@
 package com.forensic.engine.dto;
 import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RequestDTO {
     private String code;
-    private String language; // e.g., "java", "python"
+    private String language;
 }
 
 
